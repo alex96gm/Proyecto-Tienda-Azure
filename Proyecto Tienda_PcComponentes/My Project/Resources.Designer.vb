63c85221a910c57cce93bb052af9bb7d3a6a150e
alex9 <alex9@DESKTOP-1R55HHU>
2017-06-08 12:35:38 +0200

    banner 11

--- Proyecto Tienda_PcComponentes/My Project/Resources.Designer.vb
+++ Proyecto Tienda_PcComponentes/My Project/Resources.Designer.vb
@@ -123,16 +123,6 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property banner12() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("banner12", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property banner2() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("banner2", resourceCulture)
